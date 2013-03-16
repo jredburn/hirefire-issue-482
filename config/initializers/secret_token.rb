a55f186be527acbae8c8cb8e7e4436cf9ab263af
Jeremy Redburn <jredburn@salsify.com>
2013-03-15 21:54:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HirefireIssue482::Application.config.secret_token = 'e3c372f5b3cdb8f9f49b698f18731dd0315e7c522f408827cd1ae2c76826d668ba6ae6a2ffc07a9365b29169a25730cb826372ffb858d77321122fb729c2ac95'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HirefireIssue482::Application.config.session_store :cookie_store, key: '_hirefire-issue-482_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HirefireIssue482::Application.config.session_store :active_record_store
